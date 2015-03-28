34265dfe53d375dadfca99b769f18209f22326c6
MikeCoderLZ <mikeo.lz17@gmail.com>
2015-03-28 15:17:18 -0400

    Changed Boards to associate with their children as well.

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,12 +2,10 @@ class BoardsController < ApplicationController
     
     def index
         @board = Board.find( 1 )
-        @subboards = Board.where( parent: 1 )
     end
     
     def show
         @board = Board.find(params[:id])
-        @subboards = Board.where( parent: @board.id )
     end
     
     def new
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,6 @@
 class Board < ActiveRecord::Base
   belongs_to :parent, class_name: "Board"
+  has_many :children, class_name: "Board",
+           dependent: :destroy, foreign_key: "parent_id"
   
 end
