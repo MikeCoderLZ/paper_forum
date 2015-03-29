f56dd81a4bdd900207d316de536c0adf411c67cc
MikeCoderLZ <mikeo.lz17@gmail.com>
2015-03-29 16:16:54 -0400

    Added validation tests for users, indexed emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+    # copied from www.railstutorial.org, chapter 6
+    # it ain't perfect, but we can deal with that later
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255},
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: { case_sensitive: false }
+    VALID_NAME_REGEX = /\A[\w]+/
+    validates :name, presence: true,
+                     length: { minimum: 1, maximum: 255 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,64 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    
+    def setup
+        @user1 = User.new(name: "Example User", email: "user@example.com")
+    end
+    
+    # the original test was copied whole-hog from
+    # www.railstutorial.org/book/modeling_users.
+    
+    test "email validation should accept valid addresses" do
+        valid_addresses = %w[user@example.com
+                             USER@foo.COM A_US-ER@foo.bar.org
+                             first.last@foo.jp alice+bob@baz.cn]
+        valid_addresses.each do |valid_address|
+            @user1.email = valid_address
+            assert @user1.valid?, "#{valid_address.inspect} should be valid"
+        end
+    end
+    
+    # likewise
+    
+    test "email validation should reject invalid addresses" do
+        invalid_addresses = %w[user@example,com
+                               user_at_foo.org user.name@example.
+                               foo@bar_baz.com foo@bar+baz.com]
+        invalid_addresses.each do |invalid_address|
+            @user1.email = invalid_address
+            assert_not @user1.valid?,
+                       "#{invalid_address.inspect} should be invalid"
+        end
+    end
+    
+    test "emails must be unique" do
+        duplicate_user = @user1.dup
+        duplicate_user.email = @user1.email.upcase
+        @user1.save
+        assert_not duplicate_user.valid?
+    end
+    
+    test "name validation should accept limited strings as valid" do
+        valid_names = %w[ james_joyce
+                          _geofrey43
+                          HelloThere ]
+        valid_names.each do |valid_name|
+            @user1.name = valid_name
+            assert @user1.valid?,
+                  "#{valid_name.inspect} should be valid"
+        end
+    end
+    
+#     test "name validation should reject limited strings as valid" do
+#         invalid_names = %w[ james~oyce
+#                             (*^^(%__dfkjh
+#                             (){}]
+#         invalid_names.each do |invalid_name|
+#             @user1.name = invalid_name
+#             assert_not @user1.valid?,
+#                       "#{invalid_name.inspect} should be invalid"
+#         end
+#     end
+        
 end
