ca7862384b6fd2359f662f12ec4af32c94a03392
MikeCoderLZ <mikeo.lz17@gmail.com>
2015-04-10 13:53:51 -0400

    Changed icon styling to glow.

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,18 @@ Board.create( name: "Main",
               description: "Top level discussion board.",
               parent: nil,
               id: 1)
+
+User.create!(name:  "Example user",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    User.create!(name: name,
+                 email: email,
+                 password:              password,
+                 password_confirmation: password)
+end
