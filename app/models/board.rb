13bd1c4dce0bb95814322fd4da4d3bff3f6518c1
MikeCoderLZ <mikeo.lz17@gmail.com>
2015-06-01 16:38:04 -0400

    Associated boards and posts, removed post indexing

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,5 +2,6 @@ class Board < ActiveRecord::Base
   belongs_to :parent, class_name: "Board"
   has_many :children, class_name: "Board",
            dependent: :destroy, foreign_key: "parent_id"
+  has_many :topic_posts, foreign_key: "board"
   
 end
